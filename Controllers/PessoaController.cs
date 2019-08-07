8ef5047d2210ca1582e6fff266d778ac37f69295
Alexander <debarbara4@gmail.com>
2019-08-06 21:50:39 -0400

    Controllers and Actions

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreMvcControllersActions.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMvcControllersActions.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public ViewResult Index()
+        {
+            Pessoa pessoa = new Pessoa();
+
+            pessoa.Nome = "alexander";
+            pessoa.Telefone = "65 92036920";
+            return View("Pessoa", pessoa);
+        }
+        public ViewResult Formulario()
+        { 
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Formulario(Pessoa pessoa)
+        { 
+            return View("DetalhesPessoa", pessoa);
+        }
+
+    }
+}
